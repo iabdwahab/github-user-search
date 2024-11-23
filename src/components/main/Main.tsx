ce576422acedfbd2cc3ebbc1b0d46b9e5d5e2e18
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2024-11-23 04:37:00 +0200

    Implementing Date.

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -4,6 +4,7 @@ import UserInfo from './UserInfo';
 
 const Main = () => {
   const { userInfo } = useUserContext();
+  const userCreateAt = new Date(userInfo.created_at);
 
   return (
     <div className="grid md:grid-cols-[150px_1fr] gap-6 bg-medium-blue px-4 py-6 md:p-6 rounded-md">
@@ -13,7 +14,9 @@ const Main = () => {
       <div>
         <div className="md:flex md:justify-between">
           <h2 className="text-white text-3xl font-medium">{userInfo.name}</h2>
-          <p className="text-dark-grey">Joined 25 Jan 2011</p>
+          <p className="text-dark-grey">
+            {userCreateAt.getFullYear()}-{userCreateAt.getMonth() + 1}-{userCreateAt.getDate()}
+          </p>
         </div>
         <p className="text-light-blue">@{userInfo.login}</p>
         <h3 className="mt-4 text-dark-grey">{userInfo.bio}</h3>
